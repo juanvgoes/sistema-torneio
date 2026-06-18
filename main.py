b869180b096122df712f709c0397f76dbf1d173a
Juan <juan.v.goes@gmail.com>
2026-06-18 12:46:50 -0300

    adiciona cores

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+
+cor_finalizar = '\033[m'
+cor_ciano_negrito = '\033[1;36;38m'
+cor_vermelho_negrito = '\033[1;31;38m'
+cor_verde_negrito = '\033[1;32;38m'
+cor_amarelo_negrito = '\033[1;33;38m'
+cor_azul_negrito = '\033[1;34;38m'
+cor_magenta_negrito = '\033[1;35;38m'
+cor_branco_negrito = '\033[1;37;38m'
+cor_fundo_vermelho = '\033[1;31;41m'
+
 def menu():
-    print('========= Sistema de Torneios =========')
-    print('1. Formar equipes')
-    print('2. Gerar chaveamento')
-    print('3. Formar equipes e gerar chaveamento')
-    print('4. Ver histórico de chaveamentos')
-    print('5. Sair')
-    opcao = input('Escolha uma opção: ')
+    print(cor_ciano_negrito + '========= Sistema de Torneios =========' + cor_finalizar)
+    print(cor_verde_negrito + '1. Formar equipes' + cor_finalizar)
+    print(cor_amarelo_negrito + '2. Gerar chaveamento' + cor_finalizar)
+    print(cor_azul_negrito + '3. Formar equipes e gerar chaveamento' + cor_finalizar)
+    print(cor_magenta_negrito + '4. Ver histórico de chaveamentos' + cor_finalizar)
+    print(cor_vermelho_negrito + '5. Sair' + cor_finalizar)
+    opcao = input(cor_branco_negrito + 'Escolha uma opção: ' + cor_ciano_negrito)
+    print(cor_finalizar)
     return opcao
 
 if __name__ == '__main__':
@@ -13,15 +25,16 @@ if __name__ == '__main__':
         opcao = menu()
 
         if opcao == '1':
-            print('Você escolheu formar equipes')
+            print(cor_verde_negrito + 'Você escolheu formar equipes')
         elif opcao == '2':
-            print('Você escolheu gerar chaveamento')
+            print(cor_amarelo_negrito + 'Você escolheu gerar chaveamento')
         elif opcao == '3':
-            print('Você escolheu formar equipes e gerar chaveamento')
+            print(cor_azul_negrito + 'Você escolheu formar equipes e gerar chaveamento')
         elif opcao == '4':
-            print('Você escolheu ver o histórico')
+            print(cor_magenta_negrito + 'Você escolheu ver o histórico')
         elif opcao == '5':
-            print('Saindo...')
+            print(cor_vermelho_negrito + 'Saindo...' + cor_finalizar)
             break
         else:
-            print('Opção inválida, tente novamente!')
+            print(cor_fundo_vermelho + 'Opção inválida, tente novamente!' + cor_finalizar)
+        
